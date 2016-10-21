b52373731e7161f092e31d784843cff726119e9d
angshu-min-js <angshu.gupta789@gmail.com>
2016-10-22 00:35:29 +0530

    update

--- meteor/2_Meetup/both/controllers/dashboard.js
+++ meteor/2_Meetup/both/controllers/dashboard.js
@@ -11,7 +11,36 @@ DashboardController.helpers({
 });
 
 DashboardController.events({
-  'click [data-action=doSomething]': function (event, template) {
-    event.preventDefault();
-  }
+  //submit add meetups
+  'submit .update-meetup-form': function(event){
+    var title = event.target.title.value;
+    var email = event.target.email.value;
+    var topics = event.target.topics.value;
+    var type = event.target.type.value;
+    var address = event.target.address.value;
+    var city = event.target.city.value;
+    var state = event.target.state.value;
+    var zipcode = event.target.zipcode.value;
+    var meetupdate = event.target.meetupdate.value;
+    var id = event.target.id.value;
+
+    // params object and store the above
+
+    var params = {
+        title: title,
+        email: email,
+        topics: topics,
+        address: address,
+        city: city,
+        state: state,
+        zipcode: zipcode,
+        meetupdate: meetupdate,
+        updatedAt: new Date()
+    }
+
+    // insert meetups
+    Meteor.call('updateMeetup', id, params);
+    toastr.success('Meetup Updated');
+    Router.go('/dashboard');
+    return false;
 });

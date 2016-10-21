c2ccd633c238315ac82abb8bf46a3dc51d4905f3
angshu-min-js <angshu.gupta789@gmail.com>
2016-10-22 00:50:15 +0530

    update

--- meteor/2_Meetup/both/controllers/dashboard.js
+++ meteor/2_Meetup/both/controllers/dashboard.js
@@ -5,52 +5,52 @@ DashboardController = AppController.extend({
 });
 
 DashboardController.helpers({
-  'mymeetups': function(){
-    return Meetups.find({user: Meteor.userId()});
-  }
+    'mymeetups': function() {
+        return Meetups.find({user: Meteor.userId()});
+    }
 });
 
 DashboardController.events({
-  //submit add meetups
-  'submit .update-meetup-form': function(event){
-    var title = event.target.title.value;
-    var email = event.target.email.value;
-    var topics = event.target.topics.value;
-    var type = event.target.type.value;
-    var address = event.target.address.value;
-    var city = event.target.city.value;
-    var state = event.target.state.value;
-    var zipcode = event.target.zipcode.value;
-    var meetupdate = event.target.meetupdate.value;
-    var id = event.target.id.value;
+    'submit .update-meetup-form': function(event) {
+        var title = event.target.title.value;
+        var email = event.target.email.value;
+        var topics = event.target.topics.value;
+        var type = event.target.type.value;
+        var address = event.target.address.value;
+        var city = event.target.city.value;
+        var state = event.target.state.value;
+        var zipcode = event.target.zipcode.value;
+        var meetupdate = event.target.meetupdate.value;
+        var id = event.target.id.value;
 
-    // params object and store the above
+        var params = {
+            title: title,
+            email: email,
+            topics: topics,
+            address: address,
+            city: city,
+            state: state,
+            zipcode: zipcode,
+            meetupdate: meetupdate,
+            updatedAt: new Date()
+        }
 
-    var params = {
-        title: title,
-        email: email,
-        topics: topics,
-        address: address,
-        city: city,
-        state: state,
-        zipcode: zipcode,
-        meetupdate: meetupdate,
-        updatedAt: new Date()
-    }
+        // Update Meetup
+        Meteor.call('updateMeetup', id, params);
 
-    // insert meetups
-    Meteor.call('updateMeetup', id, params);
-    toastr.success('Meetup Updated');
-    Router.go('/dashboard');
-    return false;
-  },
-  'click .remove-meetup': function(event){
-    //delete meetup
-    if (confirm("Are you sure?")) {
-        // Delete Meetup
-        Meteor.call("removeMeetup", event.currentTarget.id);
-        toastr.success('Meetup Removed');
+        toastr.success('Meetup Updated');
+        $('#modal'+id).modal('hide');
+        Router.go('/dashboard');
+
         return false;
-  }
-  return false;
+    },
+    'click .remove-meetup': function(event) {
+        if (confirm("Are you sure?")) {
+            // Delete Meetup
+            Meteor.call("removeMeetup", event.currentTarget.id);
+            toastr.success('Meetup Removed');
+            return false;
+        }
+        return false;
+    }
 });

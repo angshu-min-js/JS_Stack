2816a8f4ec29e5523b0be42021a50433df9cf1f9
angshu-min-js <angshu.gupta789@gmail.com>
2016-10-22 00:37:27 +0530

    update

--- meteor/2_Meetup/server/methods/meetups.js
+++ meteor/2_Meetup/server/methods/meetups.js
@@ -1,5 +1,12 @@
 Meteor.methods({
   'addMeetup.insert': function (params) {
     Meetups.insert(params);
+  },
+  'updateMeetup': function(id, params){
+    Meteor.update({
+      _id: id
+    },{
+      $set:params
+    });
   }
 });
